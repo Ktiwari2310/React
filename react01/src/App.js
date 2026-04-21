335ea8cdcbd39c77b2eea6c141eee7ffde830a6c
Ktiwari2310 <beingkt.23@gmail.com>
2026-04-22 01:41:41 +0530

    Initialization

---
+++ react01/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>React from Basics</h1>
+  );
+}
+
+export default App;
---
+++ reactvite/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+
+
+  return (
+    <h1> Vite + React from Basics</h1>
+  )
+}
+
+export default App
