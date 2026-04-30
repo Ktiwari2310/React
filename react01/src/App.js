5c40e3dd984e5c590b0f4b4aec8b07d46f58992b
Ktiwari2310 <beingkt.23@gmail.com>
2026-04-30 18:55:15 +0530

    React basics

--- react01/src/App.js
+++ react01/src/App.js
@@ -1,8 +1,12 @@
-
+import Test from "./Test";
 
 function App() {
   return (
+    <>
     <h1>React from Basics</h1>
+    <Test />
+    </>
+    
   );
 }
 
---
+++ react01/src/Test.js
@@ -0,0 +1,12 @@
+function Test(){
+    return(
+        <>
+        <h1>This is Khushboo</h1>
+        <h2>I'm an engineering student</h2>
+        <h3>I'm learning React framework</h3>
+        </>
+
+    )
+}
+
+export default Test
