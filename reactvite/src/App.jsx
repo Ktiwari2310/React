90f7db75b2ac172ab5e08b97d56b1e4a8012b3a9
Ktiwari2310 <beingkt.23@gmail.com>
2026-04-30 18:57:45 +0530

    Vite basics

--- reactvite/src/App.jsx
+++ reactvite/src/App.jsx
@@ -1,10 +1,14 @@
-
+import Test from "./Test"
 
 function App() {
 
 
   return (
+    <>
     <h1> Vite + React from Basics</h1>
+    <Test />
+    </>
+    
   )
 }
 
---
+++ reactvite/src/Test.jsx
@@ -0,0 +1,11 @@
+function Test(){
+    return(
+        <>
+        <h1>This is Khushboo</h1>
+        <h2>I'm an engineering student</h2>
+        <h3>I'm learning React framework</h3>
+        </>
+    )
+}
+
+export default Test
